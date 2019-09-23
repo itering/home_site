2fb221a4d87e90393a007d4e5d26500a47eaf7cc
sunbobin <freehere107@hotmail.com>
2019-09-23 14:47:56 +0800

    init

---
+++ precache-manifest.6e8c962bbd3d35c91a96e5c50f8a60a3.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5adf35b90cc1185e6e6",
+    "url": "/static/js/0.69a87ead.chunk.js"
+  },
+  {
+    "revision": "670194de8b3c71ff13cd",
+    "url": "/static/css/1.7c0cd07a.chunk.css"
+  },
+  {
+    "revision": "670194de8b3c71ff13cd",
+    "url": "/static/js/1.6b672125.chunk.js"
+  },
+  {
+    "revision": "8f108b53418ae3090872",
+    "url": "/static/css/main.893b72ba.chunk.css"
+  },
+  {
+    "revision": "8f108b53418ae3090872",
+    "url": "/static/js/main.1fb8914f.chunk.js"
+  },
+  {
+    "revision": "de5898191c83a90289a2",
+    "url": "/static/js/runtime~main.ff740158.js"
+  },
+  {
+    "revision": "8c6f6e163fc5b391708b",
+    "url": "/static/js/4.425791ec.chunk.js"
+  },
+  {
+    "revision": "7ca9d527b23dd8d57942",
+    "url": "/static/css/5.c10fe23b.chunk.css"
+  },
+  {
+    "revision": "7ca9d527b23dd8d57942",
+    "url": "/static/js/5.53af1afb.chunk.js"
+  },
+  {
+    "revision": "02d3ee46eb42da455d29",
+    "url": "/static/css/6.37bd0a43.chunk.css"
+  },
+  {
+    "revision": "02d3ee46eb42da455d29",
+    "url": "/static/js/6.342fbbd3.chunk.js"
+  },
+  {
+    "revision": "2f828835a90a59aa5b34",
+    "url": "/static/css/7.6879b514.chunk.css"
+  },
+  {
+    "revision": "2f828835a90a59aa5b34",
+    "url": "/static/js/7.e47e28bb.chunk.js"
+  },
+  {
+    "revision": "05b81868c7616b931ac9",
+    "url": "/static/css/8.84fd2234.chunk.css"
+  },
+  {
+    "revision": "05b81868c7616b931ac9",
+    "url": "/static/js/8.4bf3b4e3.chunk.js"
+  },
+  {
+    "revision": "1d78b2913fe1f160425b",
+    "url": "/static/js/9.475b8b74.chunk.js"
+  },
+  {
+    "revision": "1dcabfe91ab841d8bba2737620b29ddc",
+    "url": "/static/media/Eina01SemiBold.1dcabfe9.otf"
+  },
+  {
+    "revision": "c411f608b82aaf80d9fa1ac9e316b3ce",
+    "url": "/static/media/Eina01Regular.c411f608.otf"
+  },
+  {
+    "revision": "9f04d8e29fb2c2e80892e10deea9608e",
+    "url": "/static/media/lng.9f04d8e2.svg"
+  },
+  {
+    "revision": "6b9d15b710902b0a00afd726b7c5a7d5",
+    "url": "/static/media/logo.6b9d15b7.svg"
+  },
+  {
+    "revision": "184b1bde88c9d95e1a3b0c1e576bcb1f",
+    "url": "/static/media/logo-itering.184b1bde.svg"
+  },
+  {
+    "revision": "3c216a5eb4e8326c76c197350601a656",
+    "url": "/static/media/wallet-module-banner.3c216a5e.jpg"
+  },
+  {
+    "revision": "5b063cd30133d53dab28bb97a789b1f9",
+    "url": "/static/media/home-banner.5b063cd3.png"
+  },
+  {
+    "revision": "c928280fbe7c361a5c7ec604e41e4a2a",
+    "url": "/static/media/module-1.c928280f.png"
+  },
+  {
+    "revision": "5b338647b32cc90195353cc59ccb50f9",
+    "url": "/static/media/solution-1.5b338647.png"
+  },
+  {
+    "revision": "7b6bcf384d4c8313b07537f9f98f8dcb",
+    "url": "/static/media/solution-2.7b6bcf38.png"
+  },
+  {
+    "revision": "ddac79dea959802aa1a3b21d5ff4e735",
+    "url": "/static/media/solution-3.ddac79de.png"
+  },
+  {
+    "revision": "68ae5899e679b57d29ceeea646e4e23d",
+    "url": "/static/media/about-banner.68ae5899.jpg"
+  },
+  {
+    "revision": "5db52fc38265e139f040956d8067d6b0",
+    "url": "/static/media/location-banner.5db52fc3.png"
+  },
+  {
+    "revision": "2eb3b3d41bdf8789fd21f3bb52124f4b",
+    "url": "/static/media/alex.2eb3b3d4.png"
+  },
+  {
+    "revision": "1efffb222ac5ac713c40cd59915c11de",
+    "url": "/static/media/bei.1efffb22.png"
+  },
+  {
+    "revision": "fad4dae6c690d11d01aade0d9513b58e",
+    "url": "/static/media/bruce.fad4dae6.png"
+  },
+  {
+    "revision": "6de21720eb42994a111a69888e52111a",
+    "url": "/static/media/denny.6de21720.png"
+  },
+  {
+    "revision": "0e8e001445fc9acf91c51171bc855df1",
+    "url": "/static/media/jane.0e8e0014.png"
+  },
+  {
+    "revision": "e6cd669142b358978791877388b17fa0",
+    "url": "/static/media/pengfei.e6cd6691.png"
+  },
+  {
+    "revision": "d9ccee15a0367e25dd8996d02b7621fb",
+    "url": "/static/media/shuwei.d9ccee15.png"
+  },
+  {
+    "revision": "ac83c8f5f6997055eb33818ded2bab67",
+    "url": "/static/media/shuwei1.ac83c8f5.png"
+  },
+  {
+    "revision": "5ee8aa7dbccd559fab9438d06ef5430c",
+    "url": "/static/media/tony.5ee8aa7d.png"
+  },
+  {
+    "revision": "863d76f05162feb7fab48f612d92cd60",
+    "url": "/static/media/yakio.863d76f0.png"
+  },
+  {
+    "revision": "c176fdaaf643b896f379402a7b241234",
+    "url": "/static/media/yakio1.c176fdaa.png"
+  },
+  {
+    "revision": "af238d3a0bf5fd026d7857b2de7804f8",
+    "url": "/static/media/yuqi.af238d3a.png"
+  },
+  {
+    "revision": "55013780a9880b1911ecb03b0ae6d237",
+    "url": "/static/media/proxy-bg.55013780.jpg"
+  },
+  {
+    "revision": "26705522a12f102d262e9debc6caf87f",
+    "url": "/static/media/r1.26705522.png"
+  },
+  {
+    "revision": "1d218260ccfbf8c0e3379994014da638",
+    "url": "/static/media/f2.1d218260.png"
+  },
+  {
+    "revision": "a801e3499e792ec4d8e22c3169c1b9ff",
+    "url": "/static/media/f3.a801e349.png"
+  },
+  {
+    "revision": "05ccc400ac0946f9921ec2027e16ecc5",
+    "url": "/static/media/f5.05ccc400.png"
+  },
+  {
+    "revision": "5e9e94ff418841a8903e7c695a014e86",
+    "url": "/static/media/s3.5e9e94ff.png"
+  },
+  {
+    "revision": "8063912bc53b524637f922296591bc7e",
+    "url": "/static/media/proxy-bg.8063912b.png"
+  },
+  {
+    "revision": "8ded2276b114aae0f0da598a29b69b6b",
+    "url": "/static/media/s1.8ded2276.png"
+  },
+  {
+    "revision": "a89501aabdb4a0c074ae38ee30024877",
+    "url": "/static/media/s2.a89501aa.png"
+  },
+  {
+    "revision": "9197399630bd2697eeb595dbf4445aa1",
+    "url": "/static/media/s3.91973996.png"
+  },
+  {
+    "revision": "12640d5b0e2ad590c68e893c752cb8bf",
+    "url": "/index.html"
+  }
+];
