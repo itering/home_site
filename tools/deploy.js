7afd2b61ecdf3bf06876d72f02aece295681a97b
Hongtao <acarlhong@gmail.com>
2020-04-16 10:19:16 +0800

    fix repo name

--- tools/deploy.js
+++ tools/deploy.js
@@ -4,7 +4,7 @@ var ghpages = require('gh-pages');
 export default async function() {
     ghpages.publish('build', {
         branch: 'master',
-        repo: 'git@github.com:carlhong/home_site.git'
+        repo: 'git@github.com:itering/home_site.git'
     }, function(res) {
         console.log(res)
     });
